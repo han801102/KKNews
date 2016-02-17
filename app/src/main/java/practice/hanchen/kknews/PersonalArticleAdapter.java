afab361e79bf82f7f9c35f7291813fab3549ef50
Hanchen <et801102@gmail.com>
2016-02-17 15:02:31 +0800

    Add function: let user click article title to show the content in the personal list.

--- app/src/main/java/practice/hanchen/kknews/PersonalArticleAdapter.java
+++ app/src/main/java/practice/hanchen/kknews/PersonalArticleAdapter.java
@@ -1,6 +1,7 @@
 package practice.hanchen.kknews;
 
 import android.content.Context;
+import android.view.View;
 
 import java.util.List;
 
@@ -15,5 +16,15 @@ public class PersonalArticleAdapter extends ArticleAdapter {
 	@Override
 	public void onBindViewHolder(final ViewHolder holder, final int position) {
 		super.onBindViewHolder(holder, position);
+		holder.getView().setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (holder.getLableAlbumDescription().getVisibility() == View.GONE) {
+					holder.getLableAlbumDescription().setVisibility(View.VISIBLE);
+				} else {
+					holder.getLableAlbumDescription().setVisibility(View.GONE);
+				}
+			}
+		});
 	}
 }
